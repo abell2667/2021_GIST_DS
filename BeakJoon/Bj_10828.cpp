7b5df042f58342601c19267195bee812fcaacc09
jia <abell2667@gm.gist.ac.kr>
2021-05-23 14:10:47 +0900

    HW and Bj Problems

---
+++ BeakJoon/Bj_10828.cpp
@@ -0,0 +1,19 @@
+//
+//  Bj_10828.cpp
+//  2021_GIST_DS
+//
+//  Created by 이지아 on 2021/05/14.
+//
+
+#include <iostream>
+using namespace std;
+
+//int main() {
+//    int N;
+//    cin >> N;
+//    
+//    for (int i=0; i<N; i++) {
+//        
+//    }
+//    
+//}
